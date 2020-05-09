80d428be536ef40178c51f159b54e2b3fa871836
katshaze <katrinamargaret.hayes@gmail.com>
2020-05-09 14:46:43 +1000

    fix: amend for heroku deploy

--- app.js
+++ app.js
@@ -35,9 +35,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // send the error status
   res.status(err.status || 500);
-  res.render("error");
 });
 
 module.exports = app;
